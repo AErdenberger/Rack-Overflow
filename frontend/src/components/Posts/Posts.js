5f57289c823b5bf3b4520b32c72ca72f84be0732
Josh van Eyken <josh.vaneyken@gmail.com>
2023-05-10 15:02:01 -0700

    Add limit to number of posts

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearPostErrors, fetchPosts } from '../../store/posts';
 import PostBox from './PostBox/PostBox.js';
@@ -9,11 +9,17 @@ import { Link } from 'react-router-dom';
 function Posts () {
   const dispatch = useDispatch();
   const posts = useSelector(state => Object.values(state.posts.all));
+  const [visiblePosts, setVisiblePosts] = useState(posts.slice(0, 5));
   
   useEffect(() => {
     dispatch(fetchPosts());
     return () => dispatch(clearPostErrors());
   }, [dispatch])
+
+  const loadMore = () => {
+    const nextVisiblePosts = posts.slice(0, visiblePosts.length + 5);
+    setVisiblePosts(nextVisiblePosts);
+  }
 
   if (posts.length === 0) return <div>There are no Posts</div>;
   
@@ -25,13 +31,16 @@ function Posts () {
         </div>
         <div className='posts-index'>
           <h2>All Posts</h2>
-          {posts.map(post => (
+          {visiblePosts.map(post => (
             <div key={post._id}>
               <Link to={`posts/${post._id}`}  className='posts-show-link'>
                 <PostBox  post={post} />
               </Link>
             </div>
           ))}
+          {visiblePosts.length < posts.length && (
+            <button onClick={loadMore}>Load More</button>
+          )}
         </div>
       </div>
     </>
