87e1f26a87043dc3a764db1fcefa07dc9cc6bfc2
AErdenberger <erdenberger.alexander@gmail.com>
2023-05-10 12:19:33 -0700

    move app.use above routes

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,17 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
-
-
 app.use(passport.initialize());
+
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 const usersRouter = require("./routes/api/users"); // update the import file path
 const postsRouter = require("./routes/api/posts");
@@ -52,15 +60,6 @@ if (isProduction) {
   });
 }
 
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
 
 // Attach Express routers
 
@@ -71,13 +70,13 @@ app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.statusCode = 404;
     next(err);
-  });
+});
   
   const serverErrorLogger = debug('backend:error');
   
   // Express custom error handler that will be called whenever a route handler or
   // middleware throws an error or invokes the `next` function with a truthy value
-  app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
     serverErrorLogger(err);
     const statusCode = err.statusCode || 500;
     res.status(statusCode);
@@ -86,7 +85,7 @@ app.use((req, res, next) => {
       statusCode,
       errors: err.errors
     })
-  });
+});
 
 
 module.exports = app;
