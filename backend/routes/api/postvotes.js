4b3e4d2a01c86f9d4593f3c9a07d8350cc5734d6
David Lai <pf.davidlai@gmail.com>
2023-05-16 18:11:28 -0700

    updating a vote no longer creates a new vote document, but instead updates the users original vote to update vote number

--- backend/routes/api/postvotes.js
+++ backend/routes/api/postvotes.js
@@ -7,12 +7,13 @@ const Post = mongoose.model("Post");
 const AnswerVote = mongoose.model("AnswerVote");
 const PostVote = mongoose.model("PostVote");
 
+//ALWAYS RETURN TOTAL NUMBER OF VOTES FOR ALL END POINTS
 
 router.get('/votecount/:postId', requireUser, async (req, res, next) => {
 
     const {postId} = req.params
 
-
+    let allVotesForPost = await PostVote.find()
     
 
 });
@@ -23,18 +24,34 @@ router.post('/:postId', requireUser, async (req, res, next) => {
     const {postId} = req.params
     console.log(postId,'postIDDDD')
 
+    let postVoteByUser = await PostVote.findOne({
+                                                authorId: req.user._id,
+                                                postId: postId
+                                            })
+                                            console.log(postVoteByUser,'postVOSTBYA USERSEERRRR')
+    if(postVoteByUser){
+      
+      postVoteByUser.vote = req.body.vote;
 
-
-    const postVote = new PostVote({ 
-        vote: req.body.vote,
-        authorId: req.user._id,
-        postId: postId
-    })
-
-    await postVote.save()
-            
-    return res.json(postVote)
+      await postVoteByUser.save();
     
+      
+    }
+    else{
+        const postVote = new PostVote({ 
+            vote: req.body.vote,
+            authorId: req.user._id,
+            postId: postId
+        })
+        await postVote.save()
+                
+        return awaitres.json(postVote)
+    }
+    // let allVotesForPost = await PostVote.find({
+    //                                               postId: postId
+    //                                            })
+    //  for(let userVote = 0; userVote < allVotesForPost)                                          
+    return res.json()
 });
 
 module.exports = router;
