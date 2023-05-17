92bb099fa3d0e89a114e364611406804ec21155a
David Lai <pf.davidlai@gmail.com>
2023-05-16 18:55:58 -0700

    deleted unneeded files

--- backend/routes/api/postvotes.js
+++ backend/routes/api/postvotes.js
@@ -8,22 +8,6 @@ const AnswerVote = mongoose.model("AnswerVote");
 const PostVote = mongoose.model("PostVote");
 
 //ALWAYS RETURN TOTAL NUMBER OF VOTES FOR ALL END POINTS
-// function voteTotal(postId) {
-
-//   let allPostVotes = PostVote.find({
-//                                         postId: postId
-//                                       }).exec();
-//       console.log(allPostVotes, 'allPostVotessssssss')
-//      let totalVoteCount = 0;      
-
-
-//      for(let i = 0; i < allPostVotes.length; i++){
-//         totalVoteCount += allPostVotes[i].vote;
-//      }    
-
-//      return totalVoteCount;
-// }
-
 
 async function voteTotal(postId) {
   try {
@@ -52,31 +36,29 @@ router.get('/votecount/:postId', requireUser, async (req, res, next) => {
 
 router.post('/:postId', requireUser, async (req, res, next) => {
 
-
   const { postId } = req.params
-  console.log(postId, 'postIDDDD')
 
   let postVoteByUser = await PostVote.findOne({
     authorId: req.user._id,
     postId: postId
   })
-  // console.log(postVoteByUser,'postVOSTBYA USERSEERRRR')
+
   if (postVoteByUser) {
 
-    postVoteByUser.vote = req.body.vote;
+      postVoteByUser.vote = req.body.vote;
 
-    await postVoteByUser.save();
-  }
-  else {
+      await postVoteByUser.save();
+    }
+    else {
 
-    const postVote = new PostVote({
-      vote: req.body.vote,
-      authorId: req.user._id,
-      postId: postId
-    })
-    await postVote.save()
+      const postVote = new PostVote({
+        vote: req.body.vote,
+        authorId: req.user._id,
+        postId: postId
+      })
+      await postVote.save()
 
-  }
+    }
 
   return res.json(await voteTotal(postId))
 });
