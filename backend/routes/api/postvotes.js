7da805f658975fa2178d3f40d8b704ac36a39c52
David Lai <pf.davidlai@gmail.com>
2023-05-16 18:50:17 -0700

    correctly returns postvote sum after every post execution, and does overwrite past votes for current post, and were demigods

--- backend/routes/api/postvotes.js
+++ backend/routes/api/postvotes.js
@@ -8,50 +8,77 @@ const AnswerVote = mongoose.model("AnswerVote");
 const PostVote = mongoose.model("PostVote");
 
 //ALWAYS RETURN TOTAL NUMBER OF VOTES FOR ALL END POINTS
+// function voteTotal(postId) {
+
+//   let allPostVotes = PostVote.find({
+//                                         postId: postId
+//                                       }).exec();
+//       console.log(allPostVotes, 'allPostVotessssssss')
+//      let totalVoteCount = 0;      
+
+
+//      for(let i = 0; i < allPostVotes.length; i++){
+//         totalVoteCount += allPostVotes[i].vote;
+//      }    
+
+//      return totalVoteCount;
+// }
+
+
+async function voteTotal(postId) {
+  try {
+    let allPostVotes = await PostVote.find({ postId }).exec();
+    console.log(allPostVotes, 'allPostVotes');
+    let totalVoteCount = 0;
+
+    for (let i = 0; i < allPostVotes.length; i++) {
+      totalVoteCount += allPostVotes[i].vote;
+    }
+
+    return totalVoteCount;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 router.get('/votecount/:postId', requireUser, async (req, res, next) => {
 
-    const {postId} = req.params
+  const { postId } = req.params
 
-    let allVotesForPost = await PostVote.find()
-    
+  return res.json(voteTotal(postId))
 
 });
 
 router.post('/:postId', requireUser, async (req, res, next) => {
 
-    
-    const {postId} = req.params
-    console.log(postId,'postIDDDD')
 
-    let postVoteByUser = await PostVote.findOne({
-                                                authorId: req.user._id,
-                                                postId: postId
-                                            })
-                                            console.log(postVoteByUser,'postVOSTBYA USERSEERRRR')
-    if(postVoteByUser){
-      
-      postVoteByUser.vote = req.body.vote;
+  const { postId } = req.params
+  console.log(postId, 'postIDDDD')
 
-      await postVoteByUser.save();
-    
-      
-    }
-    else{
-        const postVote = new PostVote({ 
-            vote: req.body.vote,
-            authorId: req.user._id,
-            postId: postId
-        })
-        await postVote.save()
-                
-        return awaitres.json(postVote)
-    }
-    // let allVotesForPost = await PostVote.find({
-    //                                               postId: postId
-    //                                            })
-    //  for(let userVote = 0; userVote < allVotesForPost)                                          
-    return res.json()
+  let postVoteByUser = await PostVote.findOne({
+    authorId: req.user._id,
+    postId: postId
+  })
+  // console.log(postVoteByUser,'postVOSTBYA USERSEERRRR')
+  if (postVoteByUser) {
+
+    postVoteByUser.vote = req.body.vote;
+
+    await postVoteByUser.save();
+  }
+  else {
+
+    const postVote = new PostVote({
+      vote: req.body.vote,
+      authorId: req.user._id,
+      postId: postId
+    })
+    await postVote.save()
+
+  }
+
+  return res.json(await voteTotal(postId))
 });
 
 module.exports = router;
