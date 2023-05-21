2f75b88a153883f518a8ceed7fc1ea1f2587785a
David Gudeman <davidmgudeman@gmail.com>
2023-05-21 14:49:32 -0700

    ditto

--- frontend/src/components/Posts/PostsCompose/PostCompose.js
+++ frontend/src/components/Posts/PostsCompose/PostCompose.js
@@ -1,137 +1,142 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { clearPostErrors, composePost } from '../../../store/posts';
-import PostBox from '../PostBox/PostBox';
-import jwtFetch from '../../../store/jwt'
-import './PostCompose.css';
-import TagInput from '../../Tags/TagInput';
-import { fetchAI, composeComment } from '../../../store/comments';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { clearPostErrors, composePost } from "../../../store/posts";
+import PostBox from "../PostBox/PostBox";
+import jwtFetch from "../../../store/jwt";
+import "./PostCompose.css";
+import TagInput from "../../Tags/TagInput";
+import { fetchAI, composeComment } from "../../../store/comments";
 
-function PostCompose () {
-  const [text, setText] = useState('');
-  const [title, setTitle] = useState('');
-  const [selectedTags,setSelectedTags] = useState([]);
-  const dispatch = useDispatch();
-  const author = useSelector(state => state.session.user);
-  const newPost = useSelector(state => state.posts.new);
-  const errors = useSelector(state => state.errors.posts);
+function PostCompose() {
+    const [text, setText] = useState("");
+    const [title, setTitle] = useState("");
+    const [selectedTags, setSelectedTags] = useState([]);
+    const dispatch = useDispatch();
+    const author = useSelector((state) => state.session.user);
+    const newPost = useSelector((state) => state.posts.new);
+    const errors = useSelector((state) => state.errors.posts);
 
+    useEffect(() => {
+        return () => dispatch(clearPostErrors());
+    }, [dispatch]);
 
-  useEffect(() => {
-    return () => dispatch(clearPostErrors());
-  }, [dispatch]);
+    const makeAIcomment = async (text, newPost) => {
+        console.log("TEXT", text);
+        console.log("NEWPOST", newPost);
+        console.log("HHHHAAAANNDDLE");
+        try {
+            const data = { prompt: text };
+            console.log("DATTATTTAX", data);
+            const response = await jwtFetch("/api/answers/open-ai", {
+                method: "POST",
+                body: JSON.stringify(data),
+            });
+            const ans = await response.json()
+            console.log('ans in MAKEAICOMMENT', ans);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  const makeAIcomment = async (text, newPost) => {
-    // let data = await dispatch(fetchAI(text));
-    // const response = await jwtFetch("/api/answers/open-ai", {
-    //     method: "POST",
-    //     body: JSON.stringify(text),
-    // });
-    // const ans = await response.json();
-    // console.log('ANSSSWEER', ans);
-    // dispatch(composeComment({ parentPost: newPost.id, text: data, tags: null }));
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let newPost = await dispatch(
+            composePost({ title, text, selectedTags })
+        );
 
-    console.log('HHHHAAAANNDDLE')
-    try {
-        const data = { prompt: text};
-        console.log('DATTATTTAX', data)
-        // const response = await jwtFetch("/api/answers/open-ai", {
-        //     method: "POST",
-        //     body: JSON.stringify(data),
-        // });
-        // const ans = await response.json()
-        // console.log('ans in MAKEAICOMMENT', ans);
-        
-    } catch (err) {
-        console.error(err);
-    }
-};
-// const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     let newPost = await dispatch(composePost({ title, text, selectedTags }))
-//     // makeAIcomment(
-//     //     text,
-//     //     newPost
-        
-//     // );
+        console.log("NEWPOEST", newPost);
+        await makeAIcomment(text, newPost);
+        setText("");
+        setTitle("");
+        setSelectedTags([]);
+    };
 
-//     setText("");
-//     setTitle("");
-//     setSelectedTags([]);
-// };
+    const colors = [
+        "tomato",
+        "brown",
+        "salmon",
+        "cyan",
+        "green",
+        "orange",
+        "gold",
+        "violet",
+        "pink",
+    ];
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    dispatch(composePost({ title, text, selectedTags })); 
-    setText('');
-    setTitle('');
-    setSelectedTags([]);
-  };
+    const changeColor = (e) => {
+        const randomIndex = Math.floor(Math.random() * colors.length);
+        const color = colors[randomIndex];
+        const label = document.getElementById("label-loading-post");
+        if (label) {
+            label.style.color = color;
+        }
+    };
 
-  const colors = ["tomato", "brown", "salmon", "cyan",
-    "green", "orange", "gold", "violet", "pink"
-  ]
+    setInterval(function () {
+        changeColor();
+    }, 1500);
 
-  const changeColor = e => {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    const color = colors[randomIndex];
-    const label = document.getElementById("label-loading-post");
-    if(label){
-      label.style.color = color;
-    }
-  };
-
-  setInterval(function(){
-    changeColor();
-  }, 1500);
-
-  const updateTitle = e => setTitle(e.currentTarget.value);
-  const updateText = e => setText(e.currentTarget.value);
-  const updateTags = e => setSelectedTags([e.currentTarget.value]);
-  return (
-    <div id='container-create-post-form'>
-      <form className="compose-post" onSubmit={handleSubmit}>
-        <div id='container-title'>
-          <input type='textarea' id='title'
-            value={title}
-            onChange={updateTitle}
-            placeholder='Write a title...'
-            required 
-          />
+    const updateTitle = (e) => setTitle(e.currentTarget.value);
+    const updateText = (e) => setText(e.currentTarget.value);
+    const updateTags = (e) => setSelectedTags([e.currentTarget.value]);
+    return (
+        <div id="container-create-post-form">
+            <form className="compose-post" onSubmit={handleSubmit}>
+                <div id="container-title">
+                    <input
+                        type="textarea"
+                        id="title"
+                        value={title}
+                        onChange={updateTitle}
+                        placeholder="Write a title..."
+                        required
+                    />
+                </div>
+                <div id="container-text-content">
+                    <input
+                        type="textarea"
+                        id="text-content"
+                        value={text}
+                        onChange={updateText}
+                        placeholder="Write your post..."
+                        required
+                    />
+                </div>
+                <div id="container-tags-dropmenu">
+                    <TagInput
+                        onChange={updateTags}
+                        selectedTags={selectedTags}
+                        setSelectedTags={setSelectedTags}
+                    />
+                </div>
+                <div className="errors">{errors?.text}</div>
+                <input type="submit" value="Submit" id="button-submit" />
+            </form>
+            <div className="post-preview">
+                <label id="label-post-preview">Post Preview</label>
+                <div>
+                    {text ? (
+                        <PostBox post={{ text, author, tags: selectedTags }} />
+                    ) : (
+                        <label id="label-loading-post"> Loading...</label>
+                    )}
+                </div>
+            </div>
+            <div className="previous-post">
+                <label id="label-post-previous">Previous Post</label>
+                <div>
+                    {newPost ? (
+                        <PostBox post={newPost} />
+                    ) : (
+                        <label id="label-no-previous-post">
+                            {" "}
+                            No previous post, go ahead and create one!
+                        </label>
+                    )}
+                </div>
+            </div>
         </div>
-        <div id='container-text-content'>
-          <input 
-            type="textarea" id='text-content'
-            value={text}
-            onChange={updateText}
-            placeholder="Write your post..."
-            required
-          />
-        </div>
-        <div id='container-tags-dropmenu'>
-           <TagInput 
-            onChange = {updateTags}
-            selectedTags={selectedTags}
-            setSelectedTags={setSelectedTags}
-           />
-        </div>
-        <div className="errors">{errors?.text}</div>
-        <input type="submit" value="Submit" id='button-submit' />
-      </form>
-      <div className="post-preview">
-        <label id='label-post-preview'>Post Preview</label>
-        <div>
-          {text ? <PostBox post={{text, author, tags: selectedTags}} /> : <label id='label-loading-post'> Loading...</label>}
-        </div>
-      </div>
-      <div className="previous-post">
-        <label id='label-post-previous'>Previous Post</label>
-        <div>
-          {newPost ? <PostBox post={newPost} /> : <label id='label-no-previous-post'> No previous post, go ahead and create one!</label>}
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
 export default PostCompose;
