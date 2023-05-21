07f670cd3c75b162c36f05f65211366dfd86b57c
David Gudeman <davidmgudeman@gmail.com>
2023-05-21 11:57:40 -0700

    update answers

--- backend/routes/api/answers.js
+++ backend/routes/api/answers.js
@@ -7,6 +7,23 @@ const Tag = mongoose.model("Tag");
 const { requireUser } = require("../../config/passport");
 const validateAnswerInput = require("../../validations/posts");
 
+
+
+
+router.post("/open-ai", async (req, res, next) => {
+    try {
+        const { prompt } = req.body;
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0.6,
+        });
+        res.status(200).json({ result: completion.data.choices[0].text });
+        //
+    } catch (err) {
+        next(err);
+    }
+});
 // In development, allow developers to access the CSRF token to test the
 // server endpoints in Postman.
 router.get("/restore", (req, res) => {
