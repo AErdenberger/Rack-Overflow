ca502b458043fb0c5cce5b3b88b37e5b44372ed8
David Gudeman <davidmgudeman@gmail.com>
2023-05-24 14:39:56 -0700

    add OPENAI_API_KEY to keys

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -2,6 +2,7 @@ module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY,
   mongoURI: process.env.MONGO_URI,
   isProduction: process.env.NODE_ENV === 'production',
+  openAIKey: process.env.OPENAI_API_KEY
 };
 
   
